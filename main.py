7de2ed90516497919f2732b96147b835f0481de9
HarshTourRadar <harsh.shah@tourradar.com>
2024-06-07 12:10:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ import functions
 # Check OpenAI version is correct
 required_version = version.parse("1.1.1")
 current_version = version.parse(openai.__version__)
-# OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-OPENAI_API_KEY = "sk-proj-3Mqn8wC1GLyIS5tQ9lACT3BlbkFJoez0OLP6uT5NRgLsOU7k"
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 if current_version < required_version:
     raise ValueError(
         f"Error: OpenAI version {openai.__version__}"
